b10acf7d26db5a5044ca4f4eff1d202e326f6c1c
Ritu0097 <rituchaudhary0097@gmail.com>
2024-04-17 12:49:04 +0530

    markdown

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import Mark1 from './Mark1'
-import Mark2 from './Mark2'
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
+const App = () => {
+  const [text, setText] = useState("");
+  const [loading, setLoading] = useState(true);
 
-let App= ()=>{
-    return (
-        <div className='app'>
-            <Mark1/>
-            <Mark2/>
-        </div>
-    )
-}
-export default App
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+      setText(text);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <div className="app">
+      <textarea
+        name="text"
+        value={text}
+        id="text"
+        onChange={(e) => setText(e.target.value)}
+        className="textarea"
+      ></textarea>
+
+      <div className="preview">
+        {loading ? <p className="loading">Loading...</p> : <p>{text}</p>}
+      </div>
+    </div>
+  );
+};
+
+export default App;
