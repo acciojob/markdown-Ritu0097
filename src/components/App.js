9305276aab80648c6a8c3295446e879555a1d4bd
Ritu0097 <rituchaudhary0097@gmail.com>
2024-06-14 09:16:41 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,12 @@
-import React from 'react'
-import ReactMarkdown from 'react-markdown'
-import '../styles/App.css'
-const AppComponent = () => {
-	const [markdown, setMarkdown] = React.useState('')
-	const [isLoading, setIsLoading] = React.useState(false)
-	React.useEffect(() => {
-		setIsLoading(true)
-		const timer = setTimeout(() => {
-			setIsLoading(false)
-		}, 1000) 
-		return () => clearTimeout(timer)
-	}, [markdown])
-	const handleChange = (e) => {
-		setMarkdown(e.target.value)
-	}
-	return (
-		<div className="app">
-			<textarea
-				className="textarea"
-				value={markdown}
-				onChange={handleChange}
-				placeholder="Write your markdown here..."
-			/>
-			<div className="preview">
-				{isLoading ? (
-					<p className="loading">Loading...</p>
-				) : (
-					<ReactMarkdown>{markdown}</ReactMarkdown>
-				)}
-			</div>
-		</div>
-	)
-}
-const App = () => {
-	return <AppComponent />
-}
-export default App
+import React from "react";
+import Mark1 from "./Mark1";
+import Mark2 from "./Mark2";
+
+let App = () => {
+  return (
+    <div className="app">
+      <Mark1 />
+    </div>
+  );
+};
+export default App;
